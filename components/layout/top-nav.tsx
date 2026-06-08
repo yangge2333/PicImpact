2a41c284b9583feee0148f107d9bda52b182f71e
Maiyang Lu <774202796@qq.com>
2026-06-08 16:34:38 +0800

    style: simplify top nav backdrop

--- components/layout/top-nav.tsx
+++ components/layout/top-nav.tsx
@@ -48,7 +48,7 @@ export default function TopNav(props: Readonly<AlbumDataProps>) {
   return (
     <>
       <header className="fixed inset-x-0 top-0 z-50 h-10">
-        <div className="absolute inset-x-0 top-0 h-14 bg-gradient-to-b from-white/34 via-white/18 to-transparent backdrop-blur-[14px] backdrop-saturate-150 dark:from-black/34 dark:via-black/18" />
+        <div className="absolute inset-0 bg-background/24 backdrop-blur-[12px] backdrop-saturate-150 dark:bg-black/16" />
         <nav className="relative flex h-10 items-center justify-between px-2.5 sm:px-4 lg:px-6">
           {/* Left: Site logo/name */}
           <Link
